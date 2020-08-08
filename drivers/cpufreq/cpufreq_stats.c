2ca468fcdcdf7e2827fa8bbd3cdc2851bc5eaf76
Akash <akashniki@gmail.com>
2020-08-08 16:20:49 +0200

    drivers: Fix a derp introduced here https://github.com/markakash/kernel_xiaomi_sm8150/commit/25c224b89de5f96008d5dd74f476335888e3effd

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -32,7 +32,6 @@ static void cpufreq_stats_update(struct cpufreq_stats *stats)
 
 	time = atomic64_xchg(&stats->last_time, time);
 	atomic64_add(cur_time - time, &stats->time_in_state[stats->last_index]);
-	return 0;
 }
 
 static void cpufreq_stats_clear_table(struct cpufreq_stats *stats)
